2a0ef40e281cffcef2a560e65394c352edf54675
opklmok-sys <opklmok@gmail.com>
2026-08-18 11:20:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventListener(’DOMContentLoaded‘,function () {
-    document.getElementByI('year').innerHTML = new Date().getFulIYear();
+    document.getElementById('year').innerHTML = new Date().getFulIYear();
     console.log(’Portfolio ของ kiadtisak พร้อมแล้ว!‘);
 });
